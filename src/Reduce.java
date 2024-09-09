c2e4574ba6c0ee74a7510b00101a3fefee36495b
sbocoun <sbocoun@gmail.com>
2024-09-09 12:42:10 -0400

    Added body to main method.

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,14 @@
 public class Reduce {
+    public static void main(String[] args) {
+        int count = 0;
+        int i = 100;
+        while (i > 0) {
+            count++;
+            if (i % 2 == 0) {
+                i = (i / 2);
+            } else {
+                i = (i - 1);
+            }
+        } System.out.println(count);
+    }
 }
